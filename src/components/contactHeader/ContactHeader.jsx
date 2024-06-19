5a3b7592d931d7af8420a300ccb90e1a762d15c5
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-06-19 16:16:47 +0530

    Contact Header added

---
+++ src/components/contactHeader/ContactHeader.jsx
@@ -0,0 +1,12 @@
+import styles from "./contactHeader.module.css"
+
+function ContactHeader() {
+  return (
+      <div className= { ` container ${styles.contact_section}`}>
+          <h1>Contact Us</h1>
+          <p>Contact Us page typically starts with a catchy header. This draws the visitor in and encourages them to utilize your support offers.Contact Us page typically starts with a catchy header. This draws the visitor in and encourages them to utilize your support offers.{" "}</p>
+    </div>
+  )
+}
+
+export default ContactHeader
